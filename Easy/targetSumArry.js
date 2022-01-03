694af9afe5c38dac373245c2e8b98f2d93fa76bc
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2022-01-03 23:40:35 +0530

    First Problem of alg-expt

---
+++ Easy/targetSumArry.js
@@ -0,0 +1,17 @@
+// Taking non empty array of distinct integers, need to return array of integers which gives thier sum is equals target sum 
+
+function twoNumberSum(array, targetSum) {
+	let obj = {};
+	let sumArry = [];
+	for(let i = 0; i  < array.length; i++) {
+		obj[array[i]] = i;
+	}
+	for(let j = 0; j  < array.length; j++) {
+		let b;
+		b = targetSum-array[j];
+		if(obj[b] > -1 && obj[b]!==j) {
+			sumArry.push(array[j]);
+		}
+	}
+	return sumArry
+}
