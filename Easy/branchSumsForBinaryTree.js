5d205900cd7307b22a29a098d490cca2a52597a0
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2022-01-17 11:07:10 +0530

    updated the branch sum with iteration approach

---
+++ Easy/branchSumsForBinaryTree.js
@@ -0,0 +1,58 @@
+/**
+binary tree
+returns branch sums from left to right
+branch sum => sum values of nodes from root to leaf
+ */
+// Impleted with DFS Algorithm
+// This is the class of the input root.
+// Do not edit it.
+//O(N) time complexity | O(N) space complexity
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function CalculatebranchSums(node, runningSum, sums) {
+  // Write your code here.
+  if (!node) return;
+  const newRunningSum = runningSum + node.value;
+  if (!node.left && !node.right) {
+    sums.push(newRunningSum);
+    return;
+  }
+  CalculatebranchSums(node.left, newRunningSum, sums);
+  CalculatebranchSums(node.right, newRunningSum, sums);
+}
+
+const branchSums = (node) => {
+  const sums = [];
+  CalculatebranchSums(node, 0, sums);
+  return sums;
+};
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.branchSums = branchSums;
+
+// 2nd solution with iterations
+// function branchSums(root) {
+//   let sums = [];
+//   let runningSum = root.value;
+//   let node = root;
+//   while (node !== null) {
+//     if (!node.left && !node.right) {
+//       sums.push(runningSum);
+//     } else if (!node.left) {
+//       node = node.right;
+//     } else if (!node.right) {
+//       node = node.left;
+//     } else {
+//       break;
+//     }
+//     runningSum = runningSum + node.value;
+//   }
+//   return sums;
+// }
