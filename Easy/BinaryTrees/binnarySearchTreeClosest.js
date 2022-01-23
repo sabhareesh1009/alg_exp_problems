373892bb4962abd48ec90b2959722c4a33563f95
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2022-01-23 23:28:57 +0530

    solved three number sum

--- Easy/BinaryTrees/binnarySearchTreeClosest.js
+++ Easy/BinaryTrees/binnarySearchTreeClosest.js
@@ -9,23 +9,22 @@
 // O(logN) time | O(N) space
 // Worst case O(N) time | O(N) space
 function findClosestValueHelper(tree, target, closest) {
-	if (tree === null) return closest;
-	if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
-		closest = tree.value;
-	}
-	if (target < tree.value) {
-		return findClosestValueHelper(tree.left, target, closest);
-	} else if (target > tree.value) {
-		return findClosestValueHelper(tree.right, target, closest);
-	} else {
-		return closest
-	}
+  if (tree === null) return closest;
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value;
+  }
+  if (target < tree.value) {
+    return findClosestValueHelper(tree.left, target, closest);
+  } else if (target > tree.value) {
+    return findClosestValueHelper(tree.right, target, closest);
+  } else {
+    return closest;
+  }
 }
-
 
 function findClosestValueInBst(tree, target) {
   // Write your code here.
-	return findClosestValueHelper(tree, target, tree.value)
+  return findClosestValueHelper(tree, target, tree.value);
 }
 
 // This is the class of the input tree. Do not edit.
@@ -40,35 +39,34 @@ class BST {
 /* Method 2 O(logN) time | O(1) space 
 // Worst case O(N) time | O(N) space
 */
+
 function findClosestValueInBst(tree, target) {
-    // Write your code here.
-      let currentNode = tree;
-      let closest = tree.value;
-      while(currentNode !== null) {
-          if(Math.abs(closest - target) > Math.abs(currentNode.value - target)) {
-              closest = currentNode.value;
-          }
-          if(currentNode.value > target) {
-              currentNode = currentNode.left;
-          } else if (currentNode.value < target) {
-              currentNode = currentNode.right;
-          } else {
-              break;
-          }
-      }
-      
-      return closest;
-  }
-  
-  // This is the class of the input tree. Do not edit.
-  class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
+  // Write your code here.
+  let currentNode = tree;
+  let closest = tree.value;
+  while (currentNode !== null) {
+    if (Math.abs(closest - target) > Math.abs(currentNode.value - target)) {
+      closest = currentNode.value;
+    }
+    if (currentNode.value > target) {
+      currentNode = currentNode.left;
+    } else if (currentNode.value < target) {
+      currentNode = currentNode.right;
+    } else {
+      break;
     }
   }
-  
-  // Do not edit the line below.
-  exports.findClosestValueInBst = findClosestValueInBst;
-  
+  return closest;
+}
+
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.findClosestValueInBst = findClosestValueInBst;
---
+++ Medium/Arrays/threeNumberSum.js
@@ -0,0 +1,28 @@
+/* Array with non empty integers and need to find three number sum equal to target sum; 
+need the retun tha array of all triplets in ascending order */
+
+// O(N^2) time complexity | O(N^2) space complexity
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  const tripplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left_index = i + 1;
+    let right_index = array.length - 1;
+    while (left_index < right_index) {
+      const currentSum = array[i] + array[left_index] + array[right_index];
+      if (currentSum === targetSum) {
+        tripplets.push([array[i], array[left_index], array[right_index]]);
+        left_index++;
+        right_index--;
+      } else if (currentSum < targetSum) {
+        left_index++;
+      } else {
+        right_index--;
+      }
+    }
+  }
+  return tripplets;
+}
+
+// Do not edit the line below.
+exports.threeNumberSum = threeNumberSum;
